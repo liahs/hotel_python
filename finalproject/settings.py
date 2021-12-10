a229d2bb1d46e9ac2999252066ed3d2540c33917
Shailen Chhetri <liahschhetri2@gmail.com>
2021-12-11 02:13:44 +0530

    done

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os 
-import django_heroku
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'template')
@@ -125,3 +123,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+
+import django_heroku
+django_heroku.settings(locals())
